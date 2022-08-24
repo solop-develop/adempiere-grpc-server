9375e0eb5fbcdd9307711b38af3687c60c75715f
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-08-24 15:36:38 -0400

    remove identation.

--- src/main/java/org/spin/grpc/service/PointOfSalesServiceImplementation.java
+++ src/main/java/org/spin/grpc/service/PointOfSalesServiceImplementation.java
@@ -410,19 +410,20 @@ public class PointOfSalesServiceImplementation extends StoreImplBase {
 				throw new AdempiereException("Object Request Null");
 			}
 			log.fine("Add Line for Order = " + request.getOrderLineUuid());
-			ContextManager.getContext(request.getClientRequest());
+			ContextManager.getContext(request.getClientRequest().getSessionUuid(),
+					request.getClientRequest().getLanguage(),
+					request.getClientRequest().getOrganizationUuid(),
+					request.getClientRequest().getWarehouseUuid());
 			OrderLine.Builder orderLine = updateAndConvertOrderLine(request);
 			responseObserver.onNext(orderLine.build());
 			responseObserver.onCompleted();
 		} catch (Exception e) {
 			log.severe(e.getLocalizedMessage());
-			responseObserver.onError(
-				Status.INTERNAL
+			responseObserver.onError(Status.INTERNAL
 					.withDescription(e.getLocalizedMessage())
 					.augmentDescription(e.getLocalizedMessage())
 					.withCause(e)
-					.asRuntimeException()
-			);
+					.asRuntimeException());
 		}
 	}
 	
@@ -4740,14 +4741,12 @@ public class PointOfSalesServiceImplementation extends StoreImplBase {
 		}
 		//	Quantity
 		return ConvertUtil.convertOrderLine(
-			updateOrderLine(orderLineId,
-				ValueUtil.getBigDecimalFromDecimal(request.getQuantity()),
-				ValueUtil.getBigDecimalFromDecimal(request.getPrice()),
-				ValueUtil.getBigDecimalFromDecimal(request.getDiscountRate()),
-				request.getIsAddQuantity(),
-				RecordUtil.getIdFromUuid(I_M_Warehouse.Table_Name, request.getWarehouseUuid(), null)
-			)
-		);
+				updateOrderLine(orderLineId,
+						ValueUtil.getBigDecimalFromDecimal(request.getQuantity()),
+						ValueUtil.getBigDecimalFromDecimal(request.getPrice()),
+						ValueUtil.getBigDecimalFromDecimal(request.getDiscountRate()),
+						request.getIsAddQuantity(),
+						RecordUtil.getIdFromUuid(I_M_Warehouse.Table_Name, request.getWarehouseUuid(), null)));
 	}
 	
 	/**
@@ -4771,14 +4770,11 @@ public class PointOfSalesServiceImplementation extends StoreImplBase {
 		}
 		//	Quantity
 		return ConvertUtil.convertOrderLine(
-			addOrderLine(
-				orderId,
-				RecordUtil.getIdFromUuid(I_M_Product.Table_Name, request.getProductUuid(), null),
-				RecordUtil.getIdFromUuid(I_C_Charge.Table_Name, request.getChargeUuid(), null),
-				RecordUtil.getIdFromUuid(I_M_Warehouse.Table_Name, request.getWarehouseUuid(), null),
-				ValueUtil.getBigDecimalFromDecimal(request.getQuantity())
-			)
-		);
+				addOrderLine(orderId,
+						RecordUtil.getIdFromUuid(I_M_Product.Table_Name, request.getProductUuid(), null),
+						RecordUtil.getIdFromUuid(I_C_Charge.Table_Name, request.getChargeUuid(), null),
+						RecordUtil.getIdFromUuid(I_M_Warehouse.Table_Name, request.getWarehouseUuid(), null),
+						ValueUtil.getBigDecimalFromDecimal(request.getQuantity())));
 	}
 	
 	/***
