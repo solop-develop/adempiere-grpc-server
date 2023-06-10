5a3273735ecc84e3ab0189b67ae708d00a3058a1
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2023-06-10 18:33:14 -0400

    add comments.

--- src/main/java/org/spin/base/ui/UserInterfaceConvertUtil.java
+++ src/main/java/org/spin/base/ui/UserInterfaceConvertUtil.java
@@ -1,3 +1,19 @@
+/*************************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                              *
+ * This program is free software; you can redistribute it and/or modify it           *
+ * under the terms version 2 or later of the GNU General Public License as published *
+ * by the Free Software Foundation. This program is distributed in the hope          *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied        *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ * See the GNU General Public License for more details.                              *
+ * You should have received a copy of the GNU General Public License along           *
+ * with this program; if not, write to the Free Software Foundation, Inc.,           *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                            *
+ * For the text or an alternative of this public license, you may reach us           *
+ * Copyright (C) 2018-2023 E.R.P. Consultores y Asociados, S.A. All Rights Reserved. *
+ * Contributor(s): Edwin Betancourt, EdwinBetanc0urt@outlook.com                     *
+ *************************************************************************************/
+
 package org.spin.base.ui;
 
 import org.adempiere.core.domains.models.I_AD_Ref_List;
@@ -8,6 +24,10 @@ import org.compiere.util.Util;
 import org.spin.backend.grpc.common.TreeType;
 import org.spin.base.util.ValueUtil;
 
+/**
+ * This class was created for add all convert methods for User Interface service
+ * @author Edwin Betancourt, EdwinBetanc0urt@outlook.com , https://github.com/EdwinBetanc0urt
+ */
 public class UserInterfaceConvertUtil {
 
 	public static TreeType.Builder convertTreeType(String value) {
