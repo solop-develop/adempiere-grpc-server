c42a47963cefbfee14974b96beaec71894515f1e
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2023-08-03 21:33:38 -0400

    return is null.

--- src/main/java/org/spin/grpc/service/UserInterfaceServiceImplementation.java
+++ src/main/java/org/spin/grpc/service/UserInterfaceServiceImplementation.java
@@ -2491,7 +2491,7 @@ public class UserInterfaceServiceImplementation extends UserInterfaceImplBase {
 
 				MRefList referenceList = MRefList.get(Env.getCtx(), referenceValueId, defaultValueList, null);
 				if (referenceList == null) {
-					log.fine(Msg.parseTranslation(Env.getCtx(), "@AD_Ref_List_ID@ @NotFound@"));
+					log.fine(Msg.parseTranslation(Env.getCtx(), "@AD_Ref_List_ID@ @NotFound@") + ": " + defaultValueList);
 					return builder;
 				}
 				builder = convertDefaultValueFromResult(referenceList.getValue(), referenceList.getUUID(), referenceList.getValue(), referenceList.get_Translation(MRefList.COLUMNNAME_Name));
@@ -2511,48 +2511,56 @@ public class UserInterfaceServiceImplementation extends UserInterfaceImplBase {
 				}
 
 				MLookupInfo lookupInfo = ReferenceUtil.getReferenceLookupInfo(referenceId, referenceValueId, columnName, validationRuleId);
-				if(lookupInfo != null && !Util.isEmpty(lookupInfo.QueryDirect)) {
-					String sql = MRole.getDefault(Env.getCtx(), false).addAccessSQL(lookupInfo.QueryDirect,
-							lookupInfo.TableName, MRole.SQL_FULLYQUALIFIED, MRole.SQL_RO);
-					PreparedStatement pstmt = null;
-					ResultSet rs = null;
-					try {
-						//	SELECT Key, Value, Name FROM ...
-						pstmt = DB.prepareStatement(sql.toString(), null);
-						DB.setParameter(pstmt, 1, defaultValueAsObject);
+				if(lookupInfo == null || Util.isEmpty(lookupInfo.QueryDirect)) {
+					return builder;
+				}
+				String sql = MRole.getDefault(Env.getCtx(), false).addAccessSQL(
+					lookupInfo.QueryDirect,
+					lookupInfo.TableName,
+					MRole.SQL_FULLYQUALIFIED,
+					MRole.SQL_RO
+				);
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+				try {
+					//	SELECT Key, Value, Name FROM ...
+					pstmt = DB.prepareStatement(sql.toString(), null);
+					DB.setParameter(pstmt, 1, defaultValueAsObject);
 
-						//	Get from Query
-						rs = pstmt.executeQuery();
-						if (rs.next()) {
-							//	1 = Key Column
-							//	2 = Optional Value
-							//	3 = Display Value
-							ResultSetMetaData metaData = rs.getMetaData();
-							int keyValueType = metaData.getColumnType(1);
-							Object keyValue = null;
-							if(keyValueType == Types.VARCHAR
-									|| keyValueType == Types.NVARCHAR
-									|| keyValueType == Types.CHAR
-									|| keyValueType == Types.NCHAR) {
-								keyValue = rs.getString(2);
-							} else {
-								keyValue = rs.getInt(1);
-							}
-							String uuid = null;
-							//	Validate if exist UUID
-							int uuidIndex = getColumnIndex(metaData, I_AD_Element.COLUMNNAME_UUID);
-							if(uuidIndex != -1) {
-								uuid = rs.getString(uuidIndex);
-							}
-							//	
-							builder = convertDefaultValueFromResult(keyValue, uuid, rs.getString(2), rs.getString(3));
+					//	Get from Query
+					rs = pstmt.executeQuery();
+					if (rs.next()) {
+						//	1 = Key Column
+						//	2 = Optional Value
+						//	3 = Display Value
+						ResultSetMetaData metaData = rs.getMetaData();
+						int keyValueType = metaData.getColumnType(1);
+						Object keyValue = null;
+						if(keyValueType == Types.VARCHAR
+								|| keyValueType == Types.NVARCHAR
+								|| keyValueType == Types.CHAR
+								|| keyValueType == Types.NCHAR) {
+							keyValue = rs.getString(2);
+						} else {
+							keyValue = rs.getInt(1);
 						}
-					} catch (Exception e) {
-						log.severe(e.getLocalizedMessage());
-						throw new AdempiereException(e);
-					} finally {
-						DB.close(rs, pstmt);
+						String uuid = null;
+						//	Validate if exist UUID
+						int uuidIndex = getColumnIndex(metaData, I_AD_Element.COLUMNNAME_UUID);
+						if(uuidIndex != -1) {
+							uuid = rs.getString(uuidIndex);
+						}
+						//	
+						builder = convertDefaultValueFromResult(keyValue, uuid, rs.getString(2), rs.getString(3));
 					}
+				} catch (Exception e) {
+					log.severe(e.getLocalizedMessage());
+					e.printStackTrace();
+					throw new AdempiereException(e);
+				} finally {
+					DB.close(rs, pstmt);
+					rs = null;
+					pstmt = null;
 				}
 			}
 		} else {
