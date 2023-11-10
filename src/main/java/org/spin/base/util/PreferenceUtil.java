c5ac73686197324cd16206c0c0194f8455c399e6
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2023-11-10 14:20:06 -0400

    fix comments.

--- src/main/java/org/spin/base/util/PreferenceUtil.java
+++ src/main/java/org/spin/base/util/PreferenceUtil.java
@@ -1,3 +1,17 @@
+/************************************************************************************
+ * Copyright (C) 2018-present E.R.P. Consultores y Asociados, C.A.                  *
+ * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                     *
+ * This program is free software: you can redistribute it and/or modify             *
+ * it under the terms of the GNU General Public License as published by             *
+ * the Free Software Foundation, either version 2 of the License, or                *
+ * (at your option) any later version.                                              *
+ * This program is distributed in the hope that it will be useful,                  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                     *
+ * GNU General Public License for more details.                                     *
+ * You should have received a copy of the GNU General Public License                *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
 package org.spin.base.util;
 
 import java.util.ArrayList;
@@ -11,7 +25,7 @@ import org.compiere.util.Env;
 
 public class PreferenceUtil {
 	/** Language			*/
-	public static final String 	P_LANGUAGE = "Language";
+	public static final String P_LANGUAGE = "Language";
 
 	/** Role */
 	public static final String P_ROLE = "Role";
@@ -29,6 +43,7 @@ public class PreferenceUtil {
 	public static List<String> PROPERTIES_LIST = Arrays.asList(
 		P_LANGUAGE, P_ROLE, P_CLIENT, P_ORG, P_WAREHOUSE
 	);
+
 
 	/**
 	 * Get Session Preferences
