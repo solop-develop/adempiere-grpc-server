8ae82c16a75661edd0a5119600dd510f269e74ec
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2024-03-25 00:22:04 -0400

    fix: Query with subquery and table alias.

--- src/main/java/org/spin/base/util/ReferenceUtil.java
+++ src/main/java/org/spin/base/util/ReferenceUtil.java
@@ -142,10 +142,27 @@ public class ReferenceUtil {
 				return null;
 			}
 			final String tableAlias = tableName + "_" + columnName;
-			displayColumn = displayColumn.replace(
-				"FROM " + tableName,
-				"FROM " + tableName + " AS " + tableAlias
+
+			String regexMainTable = "(\\bFROM\\b)\\s+(?<table>" + tableName + "\\b)\\s+"
+				+ "(\\bWHERE\\b|\\bORDER\\s+BY\\b|((LEFT|INNER|RIGHT|FULL|SELF|CROSS)\\s+(OUTER\\s+){0,1}){0,1}JOIN)"
+			;
+			Pattern patternMainTable = Pattern.compile(
+				regexMainTable,
+				Pattern.CASE_INSENSITIVE | Pattern.DOTALL
 			);
+			Matcher matcherMainTable = patternMainTable
+				.matcher(displayColumn);
+			if (matcherMainTable.find()) {
+				displayColumn = displayColumn.replaceAll(
+					regexMainTable,
+					"FROM " + tableName + " AS " + tableAlias + " " + matcherMainTable.group(3)
+				);
+			} else {
+				displayColumn = displayColumn.replace(
+					"FROM " + tableName,
+					"FROM " + tableName + " AS " + tableAlias
+				);
+			}
 			displayColumn = displayColumn.replace(
 				tableName + "." + baseColumnToReplace,
 				tableAlias + "." + columnName
