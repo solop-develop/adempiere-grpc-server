27b52b2cc7ff75529931fd37401828732a8e451a
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2025-01-07 16:12:22 -0400

    feat: Update display definition to 0.0.7 (#971)

--- src/main/java/org/spin/grpc/service/display_definition/DisplayDefinitionServiceLogic.java
+++ src/main/java/org/spin/grpc/service/display_definition/DisplayDefinitionServiceLogic.java
@@ -393,7 +393,7 @@ public class DisplayDefinitionServiceLogic {
 			)
 		;
 
-		displayData.gettimeLines().forEach(kanbanItem -> {
+		displayData.getTimeLines().forEach(kanbanItem -> {
 			TimelineEntry.Builder builder = DisplayDefinitionConvertUtil.convertTimelineEntry(kanbanItem);
 			builderList.addRecords(builder);
 		});
