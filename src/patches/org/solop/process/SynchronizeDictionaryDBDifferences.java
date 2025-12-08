a6bca3057423018063206afddc8252b1a71e3f97
GLescalona <gabolore@gmail.com>
2025-12-08 18:14:25 -0400

    Fix SynchronizeDictionaryDBDifferences was not using resultSet correctly (#1448)

--- src/patches/org/solop/process/SynchronizeDictionaryDBDifferences.java
+++ src/patches/org/solop/process/SynchronizeDictionaryDBDifferences.java
@@ -25,6 +25,8 @@ import org.compiere.process.ProcessInfo;
 import org.compiere.util.DB;
 import org.eevolution.services.dsl.ProcessBuilder;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /** Generated Process for (Synchronize Dictionary DataBase Differences)
  *  @author Gabriel Escalona
  *  @version Release 3.9.4
@@ -40,40 +42,62 @@ public class SynchronizeDictionaryDBDifferences extends SynchronizeDictionaryDBD
 	@Override
 	protected String doIt() throws Exception
 	{
-		//For missing Tables, Sync first column from each table
-		String sql = "SELECT DB_Element_ID FROM DictionaryInconsistency " +
-			"WHERE DB_ElementType = 'TA'";
-		DB.runResultSet(get_TrxName(),sql, null, resultSet ->{
-			String whereClause = "AD_Table_ID = ?";
-			int columnId = new Query(getCtx(), MColumn.Table_Name, whereClause, get_TrxName())
-				.setParameters(resultSet.getInt(0))
-				.setOnlyActiveRecords(true)
-				.firstId();
-			ProcessInfo processInfo = ProcessBuilder.create(getCtx())
-				.process(org.compiere.process.ColumnSync.class)
-				.withTitle("Sync Database")
-				.withRecordId(MColumn.Table_ID , columnId)
-				.execute(get_TrxName());
 
-		}).onFailure( throwable -> {throw new AdempiereException(throwable);});
-
+		AtomicInteger synchronizedTables = new AtomicInteger(0);
+		AtomicInteger totalDifferentColumnCount = new AtomicInteger(0);
+		AtomicInteger totalColumnErrorCount = new AtomicInteger(0);
 		//Mark every existing Column with Differences as RequiresSync
-		sql = "UPDATE AD_Column c SET RequiresSync = 'Y' " +
+		String sql = "UPDATE AD_Column c SET RequiresSync = 'Y' " +
 				"WHERE EXISTS (SELECT 1 FROM DictionaryInconsistency di " +
-				"WHERE di.DB_ElementType = 'CO' AND di.DB_Element_ID = c.AD_Column_ID AND di.DB_InconsistencyType <> 'NE'" +
-				")";
+				"WHERE di.DB_ElementType = 'CO' AND di.DB_Element_ID = c.AD_Column_ID)";
 		DB.executeUpdate(sql, get_TrxName());
 
-		//Sync every remaining column with differences (Exisiting and Non-Existing)
+		//For missing Tables, Sync first column from each table
+		sql = "SELECT DB_Element_ID FROM DictionaryInconsistency " +
+			"WHERE DB_ElementType = 'TA'";
+		DB.runResultSet(get_TrxName(),sql, null, resultSet ->{
+			while (resultSet.next()){
+				String whereClause = "AD_Table_ID = ?";
+				int columnId = new Query(getCtx(), MColumn.Table_Name, whereClause, get_TrxName())
+					.setParameters(resultSet.getInt(1))
+					.setOnlyActiveRecords(true)
+					.firstId();
+				ProcessInfo processInfo = ProcessBuilder.create(getCtx())
+					.process(org.compiere.process.ColumnSync.class)
+					.withTitle("Sync Database")
+					.withRecordId(MColumn.Table_ID , columnId)
+					.withoutTransactionClose()
+					.execute(get_TrxName());
+				synchronizedTables.getAndIncrement();
+				addLog(processInfo.getSummary());
+			}
+		}).onFailure( throwable -> addLog(throwable.getLocalizedMessage()));
+
+
+		//Sync every remaining column with differences (Existing and Non-Existing)
 		sql = "SELECT DB_Element_ID FROM DictionaryInconsistency " +
 				"WHERE DB_ElementType = 'CO'";
 		DB.runResultSet(get_TrxName(), sql, null, resultSet -> {
-			ProcessInfo processInfo = ProcessBuilder.create(getCtx())
-				.process(org.compiere.process.ColumnSync.class)
-				.withTitle("Sync Database")
-				.withRecordId(MColumn.Table_ID , resultSet.getInt(0))
-				.execute(get_TrxName());
-		}).onFailure(throwable -> {throw new AdempiereException(throwable);});
-		return "";
+
+			while (resultSet.next()){
+				try{
+					ProcessInfo processInfo = ProcessBuilder.create(getCtx())
+							.process(org.compiere.process.ColumnSync.class)
+							.withTitle("Sync Database")
+							.withRecordId(MColumn.Table_ID , resultSet.getInt(1))
+							.withoutTransactionClose()
+							.execute(get_TrxName());
+					totalDifferentColumnCount.getAndIncrement();
+					addLog(processInfo.getSummary());
+				} catch (Exception e) {
+					totalColumnErrorCount.getAndIncrement();
+					addLog(e.getLocalizedMessage());
+				}
+
+			}
+
+		}).onFailure(throwable -> addLog(throwable.getLocalizedMessage()));
+
+		return "@AD_Table_ID@: " + synchronizedTables.get() + " | @AD_Column_ID@: " + totalDifferentColumnCount.get() + " | @AD_Column_ID@ @Error@ " + totalColumnErrorCount.get();
 	}
 }
