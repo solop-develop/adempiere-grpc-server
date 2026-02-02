5002d719633100049c95d76ad8ecb4cd56406fc4
Gabriel Escalona <gabolore@gmail.com>
2026-02-02 12:36:17 -0400

    Update base version to 1.5.1

--- src/main/java/org/spin/grpc/service/form/match_po_receipt_invoice/MatchPOReceiptInvoiceUtil.java
+++ src/main/java/org/spin/grpc/service/form/match_po_receipt_invoice/MatchPOReceiptInvoiceUtil.java
@@ -20,6 +20,7 @@ import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
 
 import org.adempiere.core.domains.models.I_C_Invoice;
 import org.adempiere.core.domains.models.I_C_InvoiceLine;
@@ -34,19 +35,18 @@ import org.compiere.model.MMatchPO;
 import org.compiere.model.MOrderLine;
 import org.compiere.model.MPeriod;
 import org.compiere.model.MRole;
-import org.compiere.model.MStorage;
 import org.compiere.model.MSysConfig;
 import org.compiere.process.DocumentEngine;
 import org.compiere.util.CLogger;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
-// import org.solop.util.ReservationBuilder;
+import org.solop.util.ReservationBuilder;
 import org.spin.backend.grpc.form.match_po_receipt_invoice.MatchType;
 import org.spin.backend.grpc.form.match_po_receipt_invoice.Matched;
 import org.spin.backend.grpc.form.match_po_receipt_invoice.Vendor;
 import org.spin.service.grpc.util.value.NumberManager;
 import org.spin.service.grpc.util.value.TextManager;
-import org.spin.service.grpc.util.value.TimeManager;
+import org.spin.service.grpc.util.value.ValueManager;
 
 public class MatchPOReceiptInvoiceUtil {
 	/**	Logger			*/
@@ -68,28 +68,28 @@ public class MatchPOReceiptInvoiceUtil {
 		}
 
 		builder.setId(
-				businessPartner.getC_BPartner_ID()
-			)
-			.setValue(
-				TextManager.getValidString(
-					businessPartner.getValue()
+						businessPartner.getC_BPartner_ID()
 				)
-			)
-			.setTaxId(
-				TextManager.getValidString(
-					businessPartner.getTaxID()
+				.setValue(
+						TextManager.getValidString(
+								businessPartner.getValue()
+						)
 				)
-			)
-			.setName(
-				TextManager.getValidString(
-					businessPartner.getName()
+				.setTaxId(
+						TextManager.getValidString(
+								businessPartner.getTaxID()
+						)
 				)
-			)
-			.setDescription(
-				TextManager.getValidString(
-					businessPartner.getDescription()
+				.setName(
+						TextManager.getValidString(
+								businessPartner.getName()
+						)
 				)
-			)
+				.setDescription(
+						TextManager.getValidString(
+								businessPartner.getDescription()
+						)
+				)
 		;
 
 		return builder;
@@ -104,50 +104,50 @@ public class MatchPOReceiptInvoiceUtil {
 		}
 
 		builder.setId(
-				resultSet.getInt("ID")
-			)
-			.setHeaderId(
-				resultSet.getInt("Header_ID")
-			)
-			.setDate(
-				TimeManager.getProtoTimestampFromTimestamp(
-					resultSet.getTimestamp(5) // Date
+						resultSet.getInt("ID")
 				)
-			)
-			.setDocumentNo(
-				TextManager.getValidString(
-					resultSet.getString(I_C_Invoice.COLUMNNAME_DocumentNo)
+				.setHeaderId(
+						resultSet.getInt("Header_ID")
 				)
-			)
-			.setLineNo(
-				resultSet.getInt(I_C_InvoiceLine.COLUMNNAME_Line)
-			)
-			.setQuantity(
-				NumberManager.getBigDecimalToString(
-					resultSet.getBigDecimal("Quantity")
+				.setDate(
+						ValueManager.getProtoTimestampFromTimestamp(
+								resultSet.getTimestamp(5) // Date
+						)
 				)
-			)
-			.setMatchedQuantity(
-				NumberManager.getBigDecimalToString(
-					resultSet.getBigDecimal("MatchedQuantity")
+				.setDocumentNo(
+						TextManager.getValidString(
+								resultSet.getString(I_C_Invoice.COLUMNNAME_DocumentNo)
+						)
 				)
-			)
-			.setProductId(
-				resultSet.getInt(I_C_InvoiceLine.COLUMNNAME_M_Product_ID)
-			)
-			.setProductName(
-				TextManager.getValidString(
-					resultSet.getString("M_Product_Name")
+				.setLineNo(
+						resultSet.getInt(I_C_InvoiceLine.COLUMNNAME_Line)
 				)
-			)
-			.setVendorId(
-				resultSet.getInt(I_C_Invoice.COLUMNNAME_C_BPartner_ID)
-			)
-			.setVendorName(
-				TextManager.getValidString(
-					resultSet.getString("C_BPartner_Name")
+				.setQuantity(
+						NumberManager.getBigDecimalToString(
+								resultSet.getBigDecimal("Quantity")
+						)
 				)
-			)
+				.setMatchedQuantity(
+						NumberManager.getBigDecimalToString(
+								resultSet.getBigDecimal("MatchedQuantity")
+						)
+				)
+				.setProductId(
+						resultSet.getInt(I_C_InvoiceLine.COLUMNNAME_M_Product_ID)
+				)
+				.setProductName(
+						TextManager.getValidString(
+								resultSet.getString("M_Product_Name")
+						)
+				)
+				.setVendorId(
+						resultSet.getInt(I_C_Invoice.COLUMNNAME_C_BPartner_ID)
+				)
+				.setVendorName(
+						TextManager.getValidString(
+								resultSet.getString("C_BPartner_Name")
+						)
+				)
 		;
 
 		return builder;
@@ -180,56 +180,56 @@ public class MatchPOReceiptInvoiceUtil {
 		String sql = "";
 		if (matchTypeFrom == MatchType.INVOICE_VALUE) {
 			sql = "SELECT lin.C_InvoiceLine_ID AS ID, lin.UUID AS UUID, "
-				+ " hdr.C_Invoice_ID AS Header_ID, hdr.UUID AS Header_UUID, hdr.DateInvoiced AS Date,"
-				+ " hdr.C_Invoice_ID, hdr.DocumentNo, hdr.DateInvoiced, "
-				+ " bp.Name AS C_BPartner_Name, hdr.C_BPartner_ID, "
-				+ " lin.Line, lin.C_InvoiceLine_ID, "
-				+ " p.Name AS M_Product_Name, lin.M_Product_ID, "
-				+ " lin.QtyInvoiced, "
-				+ " lin.QtyInvoiced AS Quantity, SUM(COALESCE(mi.Qty, 0)) AS MatchedQuantity, "
-				+ " org.Name, hdr.AD_Org_ID "
-				+ "FROM C_Invoice hdr"
-				+ " INNER JOIN AD_Org org ON (hdr.AD_Org_ID = org.AD_Org_ID)"
-				+ " INNER JOIN C_BPartner bp ON (hdr.C_BPartner_ID = bp.C_BPartner_ID)"
-				+ " INNER JOIN C_InvoiceLine lin ON (hdr.C_Invoice_ID = lin.C_Invoice_ID)"
-				+ " INNER JOIN M_Product p ON (lin.M_Product_ID = p.M_Product_ID)"
-				+ " INNER JOIN C_DocType dt ON (hdr.C_DocType_ID = dt.C_DocType_ID AND dt.DocBaseType IN ('API', 'APC'))"
-				+ " FULL JOIN M_MatchInv mi ON (lin.C_InvoiceLine_ID = mi.C_InvoiceLine_ID) "
-				+ "WHERE hdr.DocStatus IN ('CO','CL')"
+					+ " hdr.C_Invoice_ID AS Header_ID, hdr.UUID AS Header_UUID, hdr.DateInvoiced AS Date,"
+					+ " hdr.C_Invoice_ID, hdr.DocumentNo, hdr.DateInvoiced, "
+					+ " bp.Name AS C_BPartner_Name, hdr.C_BPartner_ID, "
+					+ " lin.Line, lin.C_InvoiceLine_ID, "
+					+ " p.Name AS M_Product_Name, lin.M_Product_ID, "
+					+ " lin.QtyInvoiced, "
+					+ " lin.QtyInvoiced AS Quantity, SUM(COALESCE(mi.Qty, 0)) AS MatchedQuantity, "
+					+ " org.Name, hdr.AD_Org_ID "
+					+ "FROM C_Invoice hdr"
+					+ " INNER JOIN AD_Org org ON (hdr.AD_Org_ID = org.AD_Org_ID)"
+					+ " INNER JOIN C_BPartner bp ON (hdr.C_BPartner_ID = bp.C_BPartner_ID)"
+					+ " INNER JOIN C_InvoiceLine lin ON (hdr.C_Invoice_ID = lin.C_Invoice_ID)"
+					+ " INNER JOIN M_Product p ON (lin.M_Product_ID = p.M_Product_ID)"
+					+ " INNER JOIN C_DocType dt ON (hdr.C_DocType_ID = dt.C_DocType_ID AND dt.DocBaseType IN ('API', 'APC'))"
+					+ " FULL JOIN M_MatchInv mi ON (lin.C_InvoiceLine_ID = mi.C_InvoiceLine_ID) "
+					+ "WHERE hdr.DocStatus IN ('CO','CL')"
 			;
 		}
 		else if (matchTypeFrom == MatchType.PURCHASE_ORDER_VALUE) {
 			String lineType = matchTypeTo == MatchType.RECEIPT_VALUE ? "M_InOutLine_ID" : "C_InvoiceLine_ID";
 
 			sql = "SELECT lin.C_OrderLine_ID AS ID, lin.UUID AS UUID, "
-				+ " hdr.C_Order_ID AS Header_ID, hdr.UUID AS Header_UUID, hdr.DateOrdered AS Date,"
-				+ " hdr.C_Order_ID, hdr.DocumentNo, hdr.DateOrdered, "
-				+ " bp.Name AS C_BPartner_Name, hdr.C_BPartner_ID, "
-				+ " lin.Line, lin.C_OrderLine_ID, "
-				+ " p.Name AS M_Product_Name, lin.M_Product_ID, "
-				+ " lin.QtyOrdered, "
-				+ " lin.QtyOrdered AS Quantity, SUM(COALESCE(mo.Qty, 0)) AS MatchedQuantity, "
-				+ " org.Name, hdr.AD_Org_ID "
-				+ "FROM C_Order hdr"
-				+ " INNER JOIN AD_Org org ON (hdr.AD_Org_ID = org.AD_Org_ID)"
-				+ " INNER JOIN C_BPartner bp ON (hdr.C_BPartner_ID = bp.C_BPartner_ID)"
-				+ " INNER JOIN C_OrderLine lin ON (hdr.C_Order_ID = lin.C_Order_ID)"
-				+ " INNER JOIN M_Product p ON (lin.M_Product_ID = p.M_Product_ID)"
-				+ " INNER JOIN C_DocType dt ON (hdr.C_DocType_ID = dt.C_DocType_ID AND dt.DocBaseType = 'POO')"
-				+ " FULL JOIN M_MatchPO mo ON (lin.C_OrderLine_ID = mo.C_OrderLine_ID) "
-				+ " WHERE "
+					+ " hdr.C_Order_ID AS Header_ID, hdr.UUID AS Header_UUID, hdr.DateOrdered AS Date,"
+					+ " hdr.C_Order_ID, hdr.DocumentNo, hdr.DateOrdered, "
+					+ " bp.Name AS C_BPartner_Name, hdr.C_BPartner_ID, "
+					+ " lin.Line, lin.C_OrderLine_ID, "
+					+ " p.Name AS M_Product_Name, lin.M_Product_ID, "
+					+ " lin.QtyOrdered, "
+					+ " lin.QtyOrdered AS Quantity, SUM(COALESCE(mo.Qty, 0)) AS MatchedQuantity, "
+					+ " org.Name, hdr.AD_Org_ID "
+					+ "FROM C_Order hdr"
+					+ " INNER JOIN AD_Org org ON (hdr.AD_Org_ID = org.AD_Org_ID)"
+					+ " INNER JOIN C_BPartner bp ON (hdr.C_BPartner_ID = bp.C_BPartner_ID)"
+					+ " INNER JOIN C_OrderLine lin ON (hdr.C_Order_ID = lin.C_Order_ID)"
+					+ " INNER JOIN M_Product p ON (lin.M_Product_ID = p.M_Product_ID)"
+					+ " INNER JOIN C_DocType dt ON (hdr.C_DocType_ID = dt.C_DocType_ID AND dt.DocBaseType = 'POO')"
+					+ " FULL JOIN M_MatchPO mo ON (lin.C_OrderLine_ID = mo.C_OrderLine_ID) "
+					+ " WHERE "
 			;
-			
+
 			if (isMatched) {
 				sql += " mo." + lineType + " IS NOT NULL ";
 			} else {
- 				sql += " ( mo." + lineType + " IS NULL OR "
-					+ " (lin.QtyOrdered <> (SELECT sum(mo1.Qty) AS Qty"
-					+ " FROM m_matchpo mo1 WHERE "
-					+ " mo1.C_ORDERLINE_ID = lin.C_ORDERLINE_ID AND "
-					+ " hdr.C_ORDER_ID = lin.C_ORDER_ID AND "
-					+ " mo1." + lineType
-					+ " IS NOT NULL group by mo1.C_ORDERLINE_ID))) "
+				sql += " ( mo." + lineType + " IS NULL OR "
+						+ " (lin.QtyOrdered <> (SELECT sum(mo1.Qty) AS Qty"
+						+ " FROM m_matchpo mo1 WHERE "
+						+ " mo1.C_ORDERLINE_ID = lin.C_ORDERLINE_ID AND "
+						+ " hdr.C_ORDER_ID = lin.C_ORDER_ID AND "
+						+ " mo1." + lineType
+						+ " IS NOT NULL group by mo1.C_ORDERLINE_ID))) "
 				;
 			}
 			sql += " AND hdr.DocStatus IN ('CO', 'CL') ";
@@ -237,24 +237,24 @@ public class MatchPOReceiptInvoiceUtil {
 		// Receipt
 		else {
 			sql = "SELECT lin.M_InOutLine_ID AS ID, lin.UUID AS UUID, "
-				+ " hdr.M_InOut_ID AS Header_ID, hdr.UUID AS Header_UUID, hdr.MovementDate AS Date,"
-				+ " hdr.M_InOut_ID, hdr.DocumentNo, hdr.MovementDate, "
-				+ " bp.Name AS C_BPartner_Name, hdr.C_BPartner_ID, "
-				+ " lin.Line, lin.M_InOutLine_ID, "
-				+ " p.Name AS M_Product_Name, lin.M_Product_ID, "
-				+ " lin.MovementQty, "
-				+ " lin.MovementQty AS Quantity, SUM(COALESCE(m.Qty, 0)) AS MatchedQuantity, "
-				+ " org.Name, hdr.AD_Org_ID "
-				+ "FROM M_InOut hdr"
-				+ " INNER JOIN AD_Org org ON (hdr.AD_Org_ID = org.AD_Org_ID)"
-				+ " INNER JOIN C_BPartner bp ON (hdr.C_BPartner_ID = bp.C_BPartner_ID)"
-				+ " INNER JOIN M_InOutLine lin ON (hdr.M_InOut_ID = lin.M_InOut_ID)"
-				+ " INNER JOIN M_Product p ON (lin.M_Product_ID = p.M_Product_ID)"
-				+ " INNER JOIN C_DocType dt ON (hdr.C_DocType_ID = dt.C_DocType_ID AND dt.DocBaseType IN ('MMR', 'MMS'))"
-				+ " FULL JOIN "
-				+ (matchTypeFrom == MatchType.PURCHASE_ORDER_VALUE ? "M_MatchPO" : "M_MatchInv")
-				+ " m ON (lin.M_InOutLine_ID = m.M_InOutLine_ID) "
-				+ "WHERE hdr.DocStatus IN ('CO', 'CL') and dt.issotrx = 'N' "
+					+ " hdr.M_InOut_ID AS Header_ID, hdr.UUID AS Header_UUID, hdr.MovementDate AS Date,"
+					+ " hdr.M_InOut_ID, hdr.DocumentNo, hdr.MovementDate, "
+					+ " bp.Name AS C_BPartner_Name, hdr.C_BPartner_ID, "
+					+ " lin.Line, lin.M_InOutLine_ID, "
+					+ " p.Name AS M_Product_Name, lin.M_Product_ID, "
+					+ " lin.MovementQty, "
+					+ " lin.MovementQty AS Quantity, SUM(COALESCE(m.Qty, 0)) AS MatchedQuantity, "
+					+ " org.Name, hdr.AD_Org_ID "
+					+ "FROM M_InOut hdr"
+					+ " INNER JOIN AD_Org org ON (hdr.AD_Org_ID = org.AD_Org_ID)"
+					+ " INNER JOIN C_BPartner bp ON (hdr.C_BPartner_ID = bp.C_BPartner_ID)"
+					+ " INNER JOIN M_InOutLine lin ON (hdr.M_InOut_ID = lin.M_InOut_ID)"
+					+ " INNER JOIN M_Product p ON (lin.M_Product_ID = p.M_Product_ID)"
+					+ " INNER JOIN C_DocType dt ON (hdr.C_DocType_ID = dt.C_DocType_ID AND dt.DocBaseType IN ('MMR', 'MMS'))"
+					+ " FULL JOIN "
+					+ (matchTypeFrom == MatchType.PURCHASE_ORDER_VALUE ? "M_MatchPO" : "M_MatchInv")
+					+ " m ON (lin.M_InOutLine_ID = m.M_InOutLine_ID) "
+					+ "WHERE hdr.DocStatus IN ('CO', 'CL') and dt.issotrx = 'N' "
 			;
 		}
 		return sql;
@@ -263,26 +263,26 @@ public class MatchPOReceiptInvoiceUtil {
 	public static String getGroupBy(boolean isMatched, int matchType) {
 		if (matchType == MatchType.INVOICE_VALUE) {
 			return " GROUP BY hdr.C_Invoice_ID,hdr.DocumentNo,hdr.DateInvoiced,bp.Name,hdr.C_BPartner_ID,"
-				+ " lin.Line,lin.C_InvoiceLine_ID,p.Name,lin.M_Product_ID,lin.QtyInvoiced, org.Name, hdr.AD_Org_ID "
-				+ "HAVING "
-				+ (isMatched ? "0" : "lin.QtyInvoiced")
-				+ "<>SUM(COALESCE(mi.Qty,0))"
-			;
+					+ " lin.Line,lin.C_InvoiceLine_ID,p.Name,lin.M_Product_ID,lin.QtyInvoiced, org.Name, hdr.AD_Org_ID "
+					+ "HAVING "
+					+ (isMatched ? "0" : "lin.QtyInvoiced")
+					+ "<>SUM(COALESCE(mi.Qty,0))"
+					;
 		}
 		else if (matchType == MatchType.PURCHASE_ORDER_VALUE) {
 			return " GROUP BY hdr.C_Order_ID,hdr.DocumentNo,hdr.DateOrdered,bp.Name,hdr.C_BPartner_ID,"
-				+ " lin.Line,lin.C_OrderLine_ID,p.Name,lin.M_Product_ID,lin.QtyOrdered, org.Name, hdr.AD_Org_ID "
-				+ "HAVING "
-				+ (isMatched ? "0" : "lin.QtyOrdered")
-				+ "<>SUM(COALESCE(mo.Qty,0))";
+					+ " lin.Line,lin.C_OrderLine_ID,p.Name,lin.M_Product_ID,lin.QtyOrdered, org.Name, hdr.AD_Org_ID "
+					+ "HAVING "
+					+ (isMatched ? "0" : "lin.QtyOrdered")
+					+ "<>SUM(COALESCE(mo.Qty,0))";
 		}
 		// Receipt
 		return " GROUP BY hdr.M_InOut_ID,hdr.DocumentNo,hdr.MovementDate,bp.Name,hdr.C_BPartner_ID,"
-			+ " lin.Line,lin.M_InOutLine_ID,p.Name,lin.M_Product_ID,lin.MovementQty, org.Name, hdr.AD_Org_ID "
-			+ "HAVING "
-			+ (isMatched ? "0" : "lin.MovementQty")
-			+ "<>SUM(COALESCE(m.Qty,0))"
-		;
+				+ " lin.Line,lin.M_InOutLine_ID,p.Name,lin.M_Product_ID,lin.MovementQty, org.Name, hdr.AD_Org_ID "
+				+ "HAVING "
+				+ (isMatched ? "0" : "lin.MovementQty")
+				+ "<>SUM(COALESCE(m.Qty,0))"
+				;
 	}
 
 
@@ -307,10 +307,10 @@ public class MatchPOReceiptInvoiceUtil {
 		final String groupBy = getGroupBy(isMatched, matchTypeFrom);
 
 		final String sqlWithAccess = MRole.getDefault().addAccessSQL(
-			sql + whereClause,
-			"hdr",
-			MRole.SQL_FULLYQUALIFIED,
-			MRole.SQL_RO
+				sql + whereClause,
+				"hdr",
+				MRole.SQL_FULLYQUALIFIED,
+				MRole.SQL_RO
 		) + groupBy;
 
 		PreparedStatement pstmt = null;
@@ -341,15 +341,15 @@ public class MatchPOReceiptInvoiceUtil {
 	 * @return true if created
 	 */
 	public static boolean createMatchRecord(
-		boolean isInvoice, int inOutLineId, int lineId,
-		BigDecimal quantity, String transactionName
+			boolean isInvoice, int inOutLineId, int lineId,
+			BigDecimal quantity, String transactionName
 	) {
 		if (quantity.compareTo(Env.ZERO) == 0) {
 			return true;
 		}
 		log.fine("IsInvoice=" + isInvoice
-			+ ", M_InOutLine_ID=" + inOutLineId + ", Line_ID=" + lineId
-			+ ", Qty=" + quantity);
+				+ ", M_InOutLine_ID=" + inOutLineId + ", Line_ID=" + lineId
+				+ ", Qty=" + quantity);
 		//
 		boolean success = false;
 		MInOutLine shipmentLine = new MInOutLine (Env.getCtx(), inOutLineId, transactionName);
@@ -365,28 +365,28 @@ public class MatchPOReceiptInvoiceUtil {
 			//	Create Shipment - Invoice Link
 			if (invoiceLine.getM_Product_ID() != 0) {
 				Boolean useReceiptDateAcct = MSysConfig.getBooleanValue(
-					"MATCHINV_USE_DATEACCT_FROM_RECEIPT",
-					false,
-					invoiceLine.getAD_Client_ID()
+						"MATCHINV_USE_DATEACCT_FROM_RECEIPT",
+						false,
+						invoiceLine.getAD_Client_ID()
 				);
 				MMatchInv match = null;
 
 				MInOut shipment = shipmentLine.getParent();
 				Boolean isReceiptPeriodOpen = MPeriod.isOpen(
-					Env.getCtx(),
-					shipment.getDateAcct(),
-					shipment.getC_DocType().getDocBaseType(),
-					shipment.getAD_Org_ID(),
-					transactionName
+						Env.getCtx(),
+						shipment.getDateAcct(),
+						shipment.getC_DocType().getDocBaseType(),
+						shipment.getAD_Org_ID(),
+						transactionName
 				);
 
 				MInvoice invoice = invoiceLine.getParent();
 				Boolean isInvoicePeriodOpen = MPeriod.isOpen(
-					Env.getCtx(),
-					invoice.getDateAcct(),
-					invoice.getC_DocType().getDocBaseType(),
-					invoice.getAD_Org_ID(),
-					transactionName
+						Env.getCtx(),
+						invoice.getDateAcct(),
+						invoice.getC_DocType().getDocBaseType(),
+						invoice.getAD_Org_ID(),
+						transactionName
 				);
 
 				if (useReceiptDateAcct & isReceiptPeriodOpen) {
@@ -404,7 +404,7 @@ public class MatchPOReceiptInvoiceUtil {
 					success = true;
 				}
 				else {
-					log.warning("Inv Match not created: " + match);
+					log.log(Level.SEVERE, "Inv Match not created: " + match);
 				}
 				String invoiceDocumentStatus = match.getC_InvoiceLine().getC_Invoice().getDocStatus();
 				String inOutDocumentStatus = match.getM_InOutLine().getM_InOut().getDocStatus();
@@ -423,17 +423,17 @@ public class MatchPOReceiptInvoiceUtil {
 				MMatchPO matchPO = new MMatchPO(invoiceLine, invoiceLine.getParent().getDateAcct() , quantity);
 				matchPO.setC_InvoiceLine_ID(invoiceLine);
 				if (!matchPO.save()) {
-					log.warning("PO(Inv) Match not created: " + matchPO);
+					log.log(Level.SEVERE, "PO(Inv) Match not created: " + matchPO);
 				}
 				if (MClient.isClientAccountingImmediate()) {
-					// String mesageError = 
+					// String mesageError =
 					DocumentEngine.postImmediate(
-						matchPO.getCtx(),
-						matchPO.getAD_Client_ID(),
-						matchPO.get_Table_ID(),
-						matchPO.get_ID(),
-						true,
-						matchPO.get_TrxName()
+							matchPO.getCtx(),
+							matchPO.getAD_Client_ID(),
+							matchPO.get_Table_ID(),
+							matchPO.get_ID(),
+							true,
+							matchPO.get_TrxName()
 					);
 				}
 			}
@@ -457,33 +457,33 @@ public class MatchPOReceiptInvoiceUtil {
 			if (shipmentLine.getM_Product_ID() != 0) {
 				MMatchPO match = new MMatchPO(shipmentLine, null, quantity);
 				if (!match.save()) {
-					log.warning("PO Match not created: " + match);
+					log.log(Level.SEVERE, "PO Match not created: " + match);
 				}
 				else {
 					success = true;
 					//	Correct Ordered Qty for Stocked Products (see MOrder.reserveStock / MInOut.processIt)
 					if (shipmentLine.getProduct() != null && shipmentLine.getProduct().isStocked()) {
-						// /*
+						 /*
 						success = MStorage.add(
-							Env.getCtx(),
-							shipmentLine.getM_Warehouse_ID(),
-							shipmentLine.getM_Locator_ID(),
-							shipmentLine.getM_Product_ID(),
-							shipmentLine.getM_AttributeSetInstance_ID(),
-							orderLine.getM_AttributeSetInstance_ID(),
-							null,
-							null,
-							quantity.negate(),
-							transactionName
+								Env.getCtx(),
+								shipmentLine.getM_Warehouse_ID(),
+								shipmentLine.getM_Locator_ID(),
+								shipmentLine.getM_Product_ID(),
+								shipmentLine.getM_AttributeSetInstance_ID(),
+								orderLine.getM_AttributeSetInstance_ID(),
+								null,
+								null,
+								quantity.negate(),
+								transactionName
 						);
-						// */
-						/*
+						 */
+
 						ReservationBuilder.newInstance(Env.getCtx(), transactionName)
 							.withInOutLine(shipmentLine)
 							.withQuantity(quantity)
 							.build()
 						;
-						*/
+
 					}
 				}
 			}
@@ -493,5 +493,4 @@ public class MatchPOReceiptInvoiceUtil {
 		}
 		return success;
 	} // createMatchRecord
-
 }
