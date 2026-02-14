6b9e96d2cfc51fcc04faa1cbed98b8fe0abf9fa5
Gabriel Escalona <gabolore@gmail.com>
2026-02-14 08:39:33 -0400

    Update Documents DateAcct based on each document Date

---
+++ src/patches/org/solop/util/DocumentDateUtil.java
@@ -0,0 +1,26 @@
+package org.solop.util;
+
+import org.compiere.model.PO;
+
+/**
+ * Util class to set DateAcct equal to Document Date if necessary
+ *
+ * @author Gabriel Escalona
+ */
+public class DocumentDateUtil {
+    static final String DateAcctColumnName = "DateAcct";
+    /***
+     * Update DateAcct sets same as dateDocColumn
+     * only if dateDocColumn was changed and DateAcct was not Changed
+     * @param document the document to update DateAcct
+     * @param dateDocColumnName name of the column used as DateDoc in the Document (DateInvoiced, DateOrdered...)
+     */
+    public static void updateDateAcct(PO document, String dateDocColumnName) {
+        System.out.println("Doc Date changed: " + document.is_ValueChanged(dateDocColumnName));
+        System.out.println("Acct Date changed: " + document.is_ValueChanged(DateAcctColumnName));
+
+        if (document.is_ValueChanged(dateDocColumnName) && !document.is_ValueChanged(DateAcctColumnName)) {
+            document.set_ValueOfColumn(DateAcctColumnName, document.get_Value(dateDocColumnName));
+        }
+    }
+}
